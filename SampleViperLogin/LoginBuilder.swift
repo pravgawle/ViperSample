5b219849c2c7695115e9e3ebc0f111cbd5764cfd
Pravin G <>
2019-08-22 19:02:24 +0530

    uplaod Source codd

---
+++ SampleViperLogin/LoginBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  LoginBuilder.swift
+//  SampleViperLogin
+//
+//  Created by Pravin Gawale on 20/08/19.
+//  Copyright © 2019 Pravin G. All rights reserved.
+//
+
+import UIKit
+
+class LoginBuilder: NSObject {
+
+}
---
+++ SampleViperLogin/LoginPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  LoginPresenter.swift
+//  SampleViperLogin
+//
+//  Created by Pravin Gawale on 20/08/19.
+//  Copyright © 2019 Pravin G. All rights reserved.
+//
+
+import UIKit
+
+class LoginPresenter: NSObject {
+
+}
